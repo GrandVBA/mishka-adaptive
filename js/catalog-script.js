2999982271eb7c90ae94be3da9734543c5a3ac81
Vadim Bondarenko <46077606+GrandVBA@users.noreply.github.com>
2019-09-05 19:41:01 +0700

    Закончил верстку мобильной версии catalog.html

---
+++ js/catalog-script.js
@@ -0,0 +1,32 @@
+var navHeader = document.querySelector('.main-header__nav');
+var navToggle = document.querySelector('.main-header__toggle');
+var catalogLink = document.querySelectorAll('.catalog-products__inbasket');
+var popup = document.querySelector('.modal');
+var overlay = document.querySelector('.modal-overlay');
+var close = document.querySelector('.modal-close');
+
+catalogLink.forEach(function (entry) {
+  entry.addEventListener("click", function (event){
+    event.preventDefault();
+    popup.classList.add("modal-show");
+    overlay.classList.add('overlay-show');
+  });
+});
+
+close.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.remove('modal-show');
+  overlay.classList.remove('overlay-show');
+});
+
+navHeader.classList.remove('main-header__nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navHeader.classList.contains('main-header__nav--closed')) {
+    navHeader.classList.remove('main-header__nav--closed');
+    navHeader.classList.add('main-header__nav--opened');
+  } else {
+    navHeader.classList.add('main-header__nav--closed');
+    navHeader.classList.remove('main-header__nav--opened');
+  }
+});
