1abdcdcebe91a50405f6663137ae71c9e47af213
Vadim Bondarenko <46077606+GrandVBA@users.noreply.github.com>
2019-10-04 01:38:39 +0700

    прогресc бар пейдж

--- js/catalog-script.js
+++ js/catalog-script.js
@@ -30,3 +30,14 @@ navToggle.addEventListener('click', function() {
     navHeader.classList.remove('main-header__nav--opened');
   }
 });
+
+const progress = document.querySelector('.progress');
+
+window.addEventListener('scroll', progressBar);
+
+function progressBar(e){
+  var windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var per = windowScroll / windowHeight * 100;
+  progress.style.width = per + '%';
+};
--- js/script.js
+++ js/script.js
@@ -28,3 +28,14 @@
       navHeader.classList.remove('main-header__nav--opened');
     }
   });
+
+  const progress = document.querySelector('.progress');
+
+  window.addEventListener('scroll', progressBar);
+
+  function progressBar(e){
+    var windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var per = windowScroll / windowHeight * 100;
+    progress.style.width = per + '%';
+};
